582b519a654374e56ed36b312ab9f42a039de02c
dennerpl <denner.souzapl@gmail.com>
2021-02-20 09:06:06 -0300

    Updates

---
+++ precache-manifest.1d88f093609d906ad8c599cd551d1e87.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b7e9e612437a993014004986f7157fa",
+    "url": "/react-testing-library/index.html"
+  },
+  {
+    "revision": "7c2aa7b6cd18a78877c5",
+    "url": "/react-testing-library/static/css/main.09a6b5bc.chunk.css"
+  },
+  {
+    "revision": "347a3df7120503c47554",
+    "url": "/react-testing-library/static/js/2.f740f939.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/react-testing-library/static/js/2.f740f939.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c2aa7b6cd18a78877c5",
+    "url": "/react-testing-library/static/js/main.c582cf6f.chunk.js"
+  },
+  {
+    "revision": "4c804df2c7b1dcd4ddd5",
+    "url": "/react-testing-library/static/js/runtime-main.eab8309b.js"
+  }
+]);
